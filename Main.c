d3ddf5bd84661c532893f5e2f9af2c2c0e47b096
TheUnknown392 <rabinamahrjan392@gmail.com>
2024-11-02 16:59:01 +0545

    exploration and file/folder creation done

--- Main.c
+++ Main.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <dirent.h>
+
 
 #define char_buffer 360
-
-
-
-
+#define file_Length 50
 //TODO:
 
 
-// void createDir();
 // void deleteDir();
 // void createFile();
 // void deleteFile();
 
 void viewFile_and_Dir(DIR *dir,int mode,char inputDir[]);
-
-
+void createDir_and_File(char inputDir[], int mode);
 
 int isDots(char dirName[]);
 void options(char inputDir[]);
 DIR * inputDirectory(char inputDir[]);
 void exit_if_null(void *ptr, char *message);
+void clearInputBuffer();
 
 int main(){
     int menuChoice;
     DIR *dir=NULL;
-    char inputDir[char_buffer]={'\000'};
-    
+    char inputDir[char_buffer]={'\0'};
     dir=inputDirectory(inputDir);
 
     options(inputDir);
@@ -43,22 +39,22 @@ int main(){
             if(!(menuChoice>=1&&menuChoice<=11)){
                 printf("Please input 1-11: ");
             }
-            char ch;
-            while(ch=getchar()!='\n'&&ch!=EOF){} // clean up input buffer for fgets
+            clearInputBuffer();
         }while(!(menuChoice>=1&&menuChoice<=11));
 
         switch(menuChoice){
             case 1:
+            closedir(dir);
             dir=inputDirectory(inputDir);
             break;
             case 2:
-            //createDir();
+            createDir_and_File(inputDir,menuChoice);
             break;
             case 3:
             //deleteDir();
             break;
             case 4:
-            //createFile();
+            createDir_and_File(inputDir,menuChoice);
             break;
             case 5:
             //deleteFile();
@@ -73,29 +69,37 @@ int main(){
             options(inputDir);
             break;
             case 11:
-            exit(1);
+            exit(0);
             break;
             default:
             printf("Unknown Error");
             break;
         }
+        menuChoice=0;
     }
+    closedir(dir);
+    return 0;
 }
 
 void exit_if_null(void *ptr, char *message){
     if(ptr==NULL){
-        printf("%s",message);
-        exit(0);
+        perror(message);
+        exit(1);
     }
 }
 
 DIR* inputDirectory(char inputDir[]){
     DIR *dir=NULL;
-    printf("Input Directory: ");
-    fgets(inputDir, char_buffer, stdin);
-    inputDir[strcspn(inputDir,"\n")]='\0'; //removes the ending \n with \0. \n is there when we press enter. fgets scans the new line too
-    dir=opendir(inputDir);
-    exit_if_null(dir,"no directory found");
+    while(1){
+        printf("Input Directory: ");
+        fgets(inputDir, char_buffer, stdin);
+        inputDir[strcspn(inputDir,"\n")]='\0'; //removes the ending \n with \0. \n is there when we press enter. fgets scans the new line too
+        dir=opendir(inputDir);
+        if(dir!=NULL){
+            break;
+        }
+        perror("Cheak your directory and try again");
+    }
     return dir;
 }
 
@@ -110,29 +114,35 @@ void viewFile_and_Dir(DIR *dir,int mode,char inputDir[]){
             exit_if_null(NULL,"Problem finding some path");
         } 
         if(!isDots(entry->d_name)){
-            if(mode==6){
+            switch(mode){
+                case 6:
                 printf("%s\n",entry->d_name);
-            }else if(mode==7){
+                break;
+                case 7:
                 if(S_ISREG(pathTypeStruct.st_mode)){ // returns 1 if it's a file
                     printf("%s\n",entry->d_name);
                 }
-            }else if(mode==8){
+                break;
+                case 8:
                 if(S_ISDIR(pathTypeStruct.st_mode)){ // returns if it's a directory
                     printf("%s\n",entry->d_name);
                 }
-            }else if(mode==9){
+                break;
+                case 9:
                 if(!(S_ISREG(pathTypeStruct.st_mode)||S_ISDIR(pathTypeStruct.st_mode))){ // returns if it's a not directory or file
                     printf("%s\n",entry->d_name);
                 }
-            }else{
-                printf("Error in viewFile_and_Dir");
-                exit(0);
+                break;
+                default:
+                exit_if_null(NULL,"Error: ");
+                break;
             }
         }
     }
     rewinddir(dir);
     printf("\n");
 }
+
 void options(char inputDir[]){
     printf("\nCurrent Directory: %s\n",inputDir);
     printf("\n1. Change the directory\n2. Create directory\n3. Delete directory\n4. Create file\n5. Delete file\n6. View files and directory\n7. View files only\n8. View directory only\n9. View special directory and Files\n10. Show Main Options again\n11. Exit\n");
@@ -143,4 +153,47 @@ int isDots(char dirName[]){
         return 1;
     }
     return 0; 
+}
+void clearInputBuffer(){
+    char ch;
+    while(ch=getchar()!='\n'&&ch!=EOF){} // clean up input buffer for fgets
+}
+void createDir_and_File(char inputDir[],int mode){
+    char tempDirOrFile[char_buffer];
+    char file_folder_name[file_Length]={'\0'};
+    FILE *file;
+    
+    (mode==2)?
+    printf("What do you want to name your directory? You must follow directory naming convention: "):
+    printf("What do you want to name your file? You must follow file naming convention: ");
+
+    fgets(file_folder_name,file_Length,stdin);
+    
+    int len=strlen(file_folder_name);//fgets also inputs the enter '\n' into the array.
+    file_folder_name[len-1]='\0'; // removing '\n'
+
+    snprintf(tempDirOrFile,char_buffer,"%s/%s",inputDir,file_folder_name);
+    switch(mode){
+        case 2:
+        if (mkdir(tempDirOrFile)){
+            perror("\nAttempt at creating folder has been unsucessfull");
+        }else{
+            printf(" \nFolder creation sucessfull\n");
+        }
+        break;
+        case 4:
+        file=fopen(tempDirOrFile,"w");
+        if (file==NULL){
+            perror("\nAttempt at creating file has been unsucessfull");
+        }else{
+            fclose(file);
+            printf(" \nfile creation sucessfull\n");
+        }
+        break;
+        default:
+        printf("\nunknown Error");
+        exit(1);
+        break;
+    }
+
 }
